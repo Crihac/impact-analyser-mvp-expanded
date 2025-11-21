ee3d125d52aa99d9cc31b70a5fd241aba65b5312
Crihac <cricvishalagni2002@gmail.com>
2025-11-21 22:31:13 +0530

    deployed url updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function analyzeDiff(diff) {
   try {
-    const res = await axios.post("http://localhost:8000/analyze", { diff });
+    const res = await axios.post("https://impact-analyser-backend.onrender.com/analyze", { diff });
     return res.data;
   } catch (err) {
     console.error(err);
